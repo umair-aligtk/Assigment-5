c28c931a26586152f3c6b58287304a2ebf2d5d2a
umair-aligtk <156018837+umair-aligtk@users.noreply.github.com>
2024-04-01 17:09:57 +0500

    add

---
+++ aap.js
@@ -0,0 +1,65 @@
+// Q NO:1 
+ 
+// var multi = [[]]
+
+// Q N:2. Declare and initialize a multidimensional array
+// representing the following matrix:
+// var num =[[0,1,2,3] + '<br>',
+//           [1,0,1,2 ]+'<br>',
+//            [2,1,0,1] +'<br>',] 
+// document.write (`${num}` )
+
+
+//Q NO:3 Write a program to print numeric counting from 1 to 10.
+// for(i=1 ;i<11 ; i++){
+//     document.write(i +'<br>' )
+// }
+
+
+//  Q NO:4 Write a program to print multiplication table of any
+// number using for loop. Table number & length should be
+// taken as an input from user.
+// var userTable = prompt ('Enter table number which you want ')
+// var userLength= prompt ('Enter table length here')
+// for(i=1; i <= userLength.length ; i++ ){
+//     var result = i * userTable
+//     document.write(`${userTable} x ${i} = ${result} <br>`)
+// }
+
+
+
+// Q NO:5 Write a program to print items of the following array
+// using for loop:
+// fruits = [“apple”, “banana”, “mango”, “orange”,
+// “strawberry”]
+
+
+// var fruit  = ['apple ' ,'banana','mango','orange','strawbery'  ]
+// for (i=0  ; i < fruit.length; i++  ){
+//     document.write (`${fruit[i]} <br>`    )
+// }
+
+// for (i = 0 ; i <fruit.length ; i++){
+//     document.write(`Element at index ${i} is ${fruit[i]} <br>`)
+// }
+
+
+             // Q NO : 6. Generate the following series in your browser. See
+            // example output.
+// a. Counting: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
+// b. Reverse counting: 10, 9, 8, 7, 6, 5, 4, 3, 2, 1
+// c. Even: 0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20
+// d. Odd: 1, 3, 5, 7, 9, 11, 13, 15, 17, 19
+// e. Series: 2k, 4k, 6k, 8k, 10k, 12k, 14k, 16k, 18k, 20k
+
+
+
+// for (i = 1; i < 16 ;i++){
+//     document.write(i + '<br>')
+// }
+
+
+for (i =16 ; i<0  ; i++){
+    document.write(i )
+}
+
