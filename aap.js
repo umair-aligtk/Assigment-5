95d20a48f79f2cbb72a8b4dbd94b12a438bbb63d
umair-aligtk <156018837+umair-aligtk@users.noreply.github.com>
2024-04-04 18:40:35 +0500

    Update aap.js

--- aap.js
+++ aap.js
@@ -43,5 +43,80 @@
 //     document.write(`Element at index ${i} is ${fruit[i]} <br>`)
 // }
 
+// Q NO : 6. Generate the following series in your browser. See
+            // example output.
+// a. Counting: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
+// b. Reverse counting: 10, 9, 8, 7, 6, 5, 4, 3, 2, 1
+// c. Even: 0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20
+// d. Odd: 1, 3, 5, 7, 9, 11, 13, 15, 17, 19
+// e. Series: 2k, 4k, 6k, 8k, 10k, 12k, 14k, 16k, 18k, 20k
 
+
+//Counting
+// for (i = 1; i <=15 ;i++){
+//     document.write(i+'<br>' )
+// }
+
+// Reverse counting
+// for (i =15 ; i>=1  ; i--){
+//     document.write(i + '<br>')
+// }
+
+//Even  counting
+// for (i =0 ; i<= 20 ; i=i+2){
+//     document.write(i +'<br>')
+// }
+
+
+
+//odd  counting
+// for (i =1 ; i<= 20 ; i=i+2){
+//     document.write(i +'<br>')
+// }
+
+
+// for (i =2 ; i<= 20 ; i=i+2){
+//     document.write(i + 'K' +'<br>')
+// }
+
+//Q NO:7 
+//You have an array
+//A = [“cake”, “apple pie”, “cookie”, “chips”, “patties”]
+// Write a program to enable “search by user input” in an
+// array.
+// After searching, prompt the user whether the given item is
+// found in the list or not. Example:
+
+//   var arr = ['cake','apple pie', 'cookie', 'chips', 'patties' ]
+// var user = prompt ('Enter item name which you want').toLowerCase()
+// var  match = 'no'
+// for(i = 0 ; i < arr.length ; i ++ ){
+//     if (user==arr[i]){
+//         document.write(`${user}  is avilable at index ${i} in our bakery`)
+//         match = 'yes' 
+//         break
+   
+// }
+// }
+
+// if (match=='no') {
+//     document.write(`${user} is  not avilable in our bakery `)
+// }
+
+
+// Q NO:8. Write a program to identify the largest number in the
+// given array.
+
+// var largest = [24, 53, 78, 91, 12]
+//  var largestNumber = Math.max(...largest)
+// document.write (`Array items : ${largest} <br> The largest number is ${largestNumber}`)
+
+// Q NO :9. Write a program to identify the smallest number in the
+// given array.
+// var largest = [24, 53, 78, 91, 12]
+//  var largestNumber = Math.min(...largest)
+// document.write (`Array items : ${largest} <br> The smallest number is ${largestNumber}`)
+
+// Q no: 10. Write a program to print multiples of 5 ranging 1 to
+// 100.
   v
